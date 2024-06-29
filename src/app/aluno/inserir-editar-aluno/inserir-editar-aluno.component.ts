dc1554ebab76ddbdd9cda60fe75a2f974b888413
Alberto Kato <albertokatojr@gmail.com>
2024-06-29 10:23:47 -0300

    aluno crud base

--- src/app/aluno/inserir-editar-aluno/inserir-editar-aluno.component.ts
+++ src/app/aluno/inserir-editar-aluno/inserir-editar-aluno.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Aluno } from '../../shared/models/aluno.model';
+import { AlunoService } from '../services/aluno.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-inserir-editar-aluno',
   templateUrl: './inserir-editar-aluno.component.html',
-  styleUrl: './inserir-editar-aluno.component.css'
+  styleUrl: './inserir-editar-aluno.component.css',
 })
 export class InserirEditarAlunoComponent {
+  @ViewChild('formAluno') formAluno!: NgForm;
+  aluno: Aluno = new Aluno();
+  isEdit: boolean = false;
 
+  constructor(
+    private alunoService: AlunoService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    const idParam = this.activatedRoute.snapshot.params['id'];
+    if (idParam) {
+      this.isEdit = true;
+      const fetchedAluno = this.alunoService.buscarPorId(+idParam);
+
+      if (fetchedAluno !== undefined) {
+        this.aluno = fetchedAluno;
+      } else {
+        throw new Error('Aluno não encontrada: id=' + idParam);
+      }
+    } else {
+      this.isEdit = false;
+    }
+  }
+  handleClik(): void {
+    if (this.isEdit) {
+      this.editar();
+    } else {
+      this.inserir();
+    }
+  }
+  inserir(): void {
+    if (this.formAluno.form.valid) {
+      this.alunoService.inserir(this.aluno);
+      this.router.navigate(['/alunos']);
+    }
+  }
+  editar(): void {
+    if (this.formAluno.valid) {
+      this.alunoService.atualizar(this.aluno);
+      this.router.navigate(['/alunos']);
+    }
+  }
 }
--- src/app/aluno/listar-aluno/listar-aluno.component.ts
+++ src/app/aluno/listar-aluno/listar-aluno.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { AlunoService } from '../services/aluno.service';
+import { Aluno } from '../../shared/models/aluno.model';
 
 @Component({
   selector: 'app-listar-aluno',
   templateUrl: './listar-aluno.component.html',
-  styleUrl: './listar-aluno.component.css'
+  styleUrl: './listar-aluno.component.css',
 })
 export class ListarAlunoComponent {
-
+  constructor(private alunoService: AlunoService) {}
+  alunos: Aluno[] = [];
+  ngOnInit(): void {
+    this.alunos = this.listarTodos();
+  }
+  listarTodos(): Aluno[] {
+    return this.alunoService.listarTodos();
+  }
+  removerAluno($event: any, aluno: Aluno) {
+    $event.preventDefault();
+    if (confirm(`Deseja realmente remover a aluno ${aluno.nome}?`)) {
+      this.alunoService.remover(aluno.id!);
+      this.alunos = this.alunoService.listarTodos();
+    }
+  }
 }
